693b14cd2752db362c9a988ed890de067a71d9a5
hong xiem <nghia.nguyenkhac@apicoorobotic.com>
2025-03-12 16:55:44 +0700

    edit kinematics

--- gripper/mujoco/susgrip_2f/susgrip_2f_invert.py
+++ gripper/mujoco/susgrip_2f/susgrip_2f_invert.py
@@ -11,25 +11,30 @@ data = mujoco.MjData(model)
 t = 0
 def handle():
   global t
-  t+=1
-  buff = t%1300
-  dis = buff/10
+  t += 2
+  t = t % 1300
+  dis = t/10
+
   radian = math.acos((63.45-dis)/108)
   yF = math.sqrt(2916-(31.725-dis/2)**2)+3.5
   slider = (57.5-yF)/1000
   buff = -math.pi/2+radian
-  data.qpos[0] = -dis/2000 + 0.0325
-  data.qpos[1] = -buff
-  data.qpos[2] = 2*buff
-  data.qpos[3] = 2*buff
-  data.qpos[4] = slider
-  data.qpos[5] = -buff
-  data.qpos[6] = dis/2000 - 0.0325
+  
+  data.qpos[0] = -(-dis+65)/2000    # finger_outer_l_joint
+  data.qpos[1] = -buff              # inner_slider_l_joint
+  data.qpos[2] = 2*buff             # base_pad_l_joint
+  data.qpos[3] = 2*buff             # base_pad_link_l_joint             
+  data.qpos[4] = -buff           # outer_slider_l_joint
+  data.qpos[5] = slider            # slider_l_joint
+  
+  data.qpos[6] = (-dis+65)/2000
   data.qpos[7] = buff
   data.qpos[8] = -2*buff
   data.qpos[9] = -2*buff
-  data.qpos[10] = slider
-  data.qpos[11] = buff
+  data.qpos[10] = buff
+  data.qpos[11] = slider
+  
+  print(data.qpos)
   
 with mujoco.viewer.launch_passive(model, data) as viewer:
   # Close the viewer automatically after 30 wall-seconds.
